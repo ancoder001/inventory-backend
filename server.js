2a2af3c1083541af182519894de3372cf0c024aa
aravind8204 <aravindhanvgc@gmail.com>
2025-01-30 19:06:30 +0530

    route update

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cors())
 app.use(express.urlencoded({extended:true}));
 
-app.use("/",Route);
+app.use("/api",Route);
 
 // app.use("/inventory",inventoryRoute);
 // app.use("/bill",billRoute);
